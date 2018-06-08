1d7228c8eef2df4101b6721395713f4f1897b97b
王锴 <645182907@qq.com>
2018-06-08 15:18:18 +0800

    加了//

--- UITableView/ViewController.swift
+++ UITableView/ViewController.swift
@@ -8,12 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UIPickerViewDataSource  {
+    var areas = ["民兴趣","兰州","三明市","西宁去","广州市","闽侯县","海尔滨市","临汾市","汕头市","长沙去","成都市"]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return areas.count
+    }
+
+    
+    /*
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        let cell = tableView.dequeueReusableCell(withIdentifier:"Cell", for: indexPath)
+        
+        cell.textLabel?.text = areas[indexPath.row]
+        
+        //return cell
+    }
+    */
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
